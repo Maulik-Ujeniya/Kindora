d13e369f81ceba01e4c26ac587731a3509ad083d
Maulik-Ujeniya <228997825+Maulik-Ujeniya@users.noreply.github.com>
2026-07-22 14:44:54 +0530

    feat(core): add home page routing

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("apps.accounts.urls")),
+    path("", include("apps.core.urls")),
+    path("accounts/", include("apps.accounts.urls")),
 ]
