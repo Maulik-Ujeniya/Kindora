87fc8e1d0e1470869f26e3494b125550f453072a
Maulik-Ujeniya <228997825+Maulik-Ujeniya@users.noreply.github.com>
2026-07-22 16:35:39 +0530

    feat(auth): connect login and register forms

--- apps/accounts/views/auth.py
+++ apps/accounts/views/auth.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from apps.accounts.forms.login import LoginForm
+from apps.accounts.forms.register import RegisterForm
 
 def login_view(request):
-    return render(request, "accounts/login.html")
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            return redirect("dashboard")
+
+    else:
+        form = LoginForm()
+
+    return render(
+        request,
+        "accounts/login.html",
+        {"form": form},
+    )
 
 def register_view(request):
-    return render(request, "accounts/register.html")
+    form = RegisterForm()
+    return render(
+        request,
+        "accounts/register.html",
+        {"form": form},
+    )
 
 def logout_view(request):
     return HttpResponse("Logout Page")
